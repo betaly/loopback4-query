714b5229f4b428794fd4f72c55487a1ae8980fc6
TY <towyuan@outlook.com>
2023-09-14 22:47:34 +0800

    feat(repository): Add query enhanced repository classes (main)
    Added query enhanced repository classes with default CRUD methods
    overwritten. These classes include:
    - QueryEnhancedCrudRepository
    - QueryEnhancedTransactionalCrudRepository
    - CrudRepositoryWithQuery
    - CrudTransactionalRepositoryWithQuery
    
    These classes provide enhanced querying capabilities for the
    repository. The default CRUD methods are overwritten to include
    query functionality.

--- src/repository.ts
+++ src/repository.ts
@@ -3,6 +3,9 @@ import {DefaultCrudRepository, DefaultTransactionalRepository, Entity} from '@lo
 import {mixinQuery} from './decorators';
 import {QueryEnhancedRepository} from './mixins';
 
+/**
+ * Query enhanced repository with default CRUD methods overwritten
+ */
 @mixinQuery(true)
 export class QueryEnhancedCrudRepository<
   T extends Entity,
@@ -14,13 +17,44 @@ export class QueryEnhancedCrudRepository<
 export interface QueryEnhancedCrudRepository<T extends Entity, ID, Relations extends object = {}>
   extends QueryEnhancedRepository<T, Relations> {}
 
+/**
+ * Query enhanced transactional repository with default CRUD methods overwritten
+ */
 @mixinQuery(true)
-export class QueryEnhancedTransactionalRepository<
+export class QueryEnhancedTransactionalCrudRepository<
   T extends Entity,
   ID,
   Relations extends object = {},
 > extends DefaultTransactionalRepository<T, ID, Relations> {}
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export interface QueryEnhancedTransactionalRepository<T extends Entity, ID, Relations extends object = {}>
+export interface QueryEnhancedTransactionalCrudRepository<T extends Entity, ID, Relations extends object = {}>
+  extends QueryEnhancedRepository<T, Relations> {}
+
+/**
+ * Query enhanced repository without overwriting the default CRUD methods
+ */
+@mixinQuery()
+export class CrudRepositoryWithQuery<T extends Entity, ID, Relations extends object = {}> extends DefaultCrudRepository<
+  T,
+  ID,
+  Relations
+> {}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export interface CrudRepositoryWithQuery<T extends Entity, ID, Relations extends object = {}>
+  extends QueryEnhancedRepository<T, Relations> {}
+
+/**
+ * Query enhanced transactional repository without overwriting the default CRUD methods
+ */
+@mixinQuery()
+export class CrudTransactionalRepositoryWithQuery<
+  T extends Entity,
+  ID,
+  Relations extends object = {},
+> extends DefaultTransactionalRepository<T, ID, Relations> {}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export interface CrudTransactionalRepositoryWithQuery<T extends Entity, ID, Relations extends object = {}>
   extends QueryEnhancedRepository<T, Relations> {}

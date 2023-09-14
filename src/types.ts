9253bb0970bea3049f312238867179308cd1c043
TY <towyuan@outlook.com>
2023-09-14 22:50:03 +0800

    chore(types): Add NestedKeyOf type definition (main)

--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,9 @@ export type WhereValue = any;
 export type IdSort = boolean | 'numericIdOnly';
 
 export type EntityClass<T extends Entity = Entity> = typeof Entity & {prototype: T};
+
+export type NestedKeyOf<ObjectType extends object> = {
+  [Key in keyof ObjectType & (string | number)]: ObjectType[Key] extends object
+    ? `${Key}` | `${Key}.${NestedKeyOf<ObjectType[Key]>}`
+    : `${Key}`;
+}[keyof ObjectType & (string | number)];
